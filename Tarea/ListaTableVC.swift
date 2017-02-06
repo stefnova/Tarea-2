639fa51e63f8c95a349833bfe2b7c3cf403f9319
Rocío Córdova <stefnova@la-imac-de-rocio.local>
2017-02-06 00:29:47 -0500

    Pequeñas modificaciones queda pendiente la selección múltiple

---
+++ Tarea/ListaTableVC.swift
@@ -0,0 +1,133 @@
+//
+//  ListaTableVC.swift
+//  Tarea
+//
+//  Created by Rocío Córdova on 5/02/17.
+//  Copyright © 2017 Rocío Córdova. All rights reserved.
+//
+
+import UIKit
+
+class ListaTableVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    */
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let indexPaths: [IndexPath]! = tableView.indexPathsForSelectedRows
+        
+        for n in indexPaths! {
+            
+            
+            
+        }
+    }
+    
+    /*
+ 
+     let indexPathNombre = IndexPath(row: 0 , section: 0)
+     
+     let primeraCelda = tableView.cellForRow(at: indexPathNombre)
+     
+     let indexPathCelular = IndexPath(row:1, section: 0)
+     
+     let segundaCelda = tableView.cellForRow(at: indexPathCelular)
+     
+     
+     
+     let nombCompleto = primeraCelda?.textLabel?.text
+     
+     let numeContacto = segundaCelda?.detailTextLabel?.text
+     
+     var reubicacion = ""
+     
+     if lblswitch.isOn {
+     reubicacion = "Si"
+     }else{
+     reubicacion = "No"
+     }
+     
+     let viewController = segue.destination as! ViewController2
+     
+     viewController.nombreCompleto = nombCompleto!
+     viewController.numeroContacto = numeContacto!
+     viewController.reubicacion = reubicacion
+     */
+
+}
